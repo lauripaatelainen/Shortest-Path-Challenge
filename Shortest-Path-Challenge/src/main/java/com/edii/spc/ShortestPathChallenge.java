3b2410fde57a16fd89a7541da8d5f12b10a2715e
edii <edii@localhost.localdomain>
2018-09-09 22:45:20 +0300

    Netbeans-projekti luotu

---
+++ Shortest-Path-Challenge/src/main/java/com/edii/spc/ShortestPathChallenge.java
@@ -0,0 +1,9 @@
+package com.edii.spc;
+
+public class ShortestPathChallenge {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    }
+}
